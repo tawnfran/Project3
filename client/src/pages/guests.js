601b19f2cdcd57dec206b023eaeeb348907d33c4
Leah Kurtz <lfkurtz7@gmail.com>
2019-12-23 13:05:04 -0500

    pages all linked correctly

--- client/src/pages/guests.js
+++ client/src/pages/guests.js
@@ -2,15 +2,18 @@ import React from "react";
 
 function guests() {
     return (
-        <div>
-        <h1>guests page</h1>
-
-
-
-
-        </div> 
+        <div className="container">
+            <div className="list-group">
+                <a href="#" className="list-group-item list-group-item-action active">
+                    Cras justo odio
+                </a>
+                <a href="#" className="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+                <a href="#" className="list-group-item list-group-item-action">Morbi leo risus</a>
+                <a href="#" className="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+                <a href="#" className="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Vestibulum at eros</a>
+            </div>
+        </div>
     )
 }
-
 
 export default guests
