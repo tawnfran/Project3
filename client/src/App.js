56ab16c68d355fb01e56bcbebcda98e963b1fe04
Melissa Knapp <melknapp@gmail.com>
2019-12-23 12:03:56 -0500

    Trying to work on Passport

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-// import React from 'react';
+import React from 'react';
 // import guests from "./pages/guests";
 // import registry from "./pages/registry";
 // import toDo from "./pages/toDo";
-// import './App.css';
+import './App.css';
+import Wrapper from "./pages/Wrapper";
 
-//     function App() {
-//       return (
-//         // <Router>
-//     //     //   <div>
-//     //     //     <Navbar />
-//     //     //     <Wrapper>
-//     //     //       <Route exact path="/" component={home} />
-//     //     //       <Route exact path="/guests" component={guests} />
-//     //     //       <Route exact path="/registry" component={registry} />
-//     //     //       <Route exact path="/toDo" component={toDo} />
-//     //     //     </Wrapper>
-//     //     //     <Footer />
-//     //     //   </div>
-//     //     // </Router>
-//     //   );
-//     // }
+    function App() {
+      return (
+        <Router>
+          <div>
+           <Navbar />
+            <Wrapper>
+               <Route exact path="/" component={home} />
+               {/* <Route exact path="/guests" component={guests} />
+              <Route exact path="/registry" component={registry} />
+               <Route exact path="/toDo" component={toDo} /> */}
+             </Wrapper>
+             <Footer />
+           </div>
+         </Router>
+      );
+    }
 
 
-// export default App;
+export default App;
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const passport = require("passport");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+
 require("dotenv").config();
 
 var models = require("./models");
@@ -9,12 +14,31 @@ var models = require("./models");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+ 
+app.use(passport.initialize());
+ 
+app.use(passport.session()); // persistent login sessions
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+
+app.get('/welcome', function(req, res) {
+ 
+  res.send('Welcome to Passport with Sequelize');
+
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
