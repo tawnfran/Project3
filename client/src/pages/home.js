27da2ad7dd69bfe3160521cbd1f4e9edf49eff60
Leah Kurtz <lfkurtz7@gmail.com>
2020-01-06 10:10:22 -0500

    CSS edits

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,7 +4,25 @@ import React from "react";
 function home() {
   return (
     <div className= "container">
-      <h1>hshshs</h1>
+      <div class="card-body" id="signinCardbody">
+        <form id="signup" name="signin" method="post" action="signin">
+          <div class="form-group">
+            <h1>Save The Date</h1>
+            <h5>Log-In</h5>
+            <label for="username">User Name</label>
+                    <input name="username" type="text" />
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password&nbsp;&nbsp;&nbsp;</label>
+                    <input name="password" type="password" />
+
+                </div>
+
+                <span><input class="btn-warning" class="form-control" type="submit" value="Log-In" /></span>
+                 <a class="btn btn-warning btn-sm" href="/signup">Sign Up</a>
+        </form>
+      </div>
     </div>
   );
 }
